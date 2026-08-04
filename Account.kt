ddb9e513823bd45ea17f43c13271e444e504ef8f
vienyevelyn <88517941+vienyevelyn@users.noreply.github.com>
2026-08-04 10:22:24 +0700

    create function Login for validation input login

--- Account.kt
+++ Account.kt
@@ -3,4 +3,11 @@ class Account(
     val nim: String,
     private val password: String,
     private var balance: Int = 100000
-)
+){
+    fun login(inputNim: String, inputPassword: String) : Boolean{
+        if (inputNim == password && inputPassword == password){
+            return true
+        }
+        return false
+    }
+}
