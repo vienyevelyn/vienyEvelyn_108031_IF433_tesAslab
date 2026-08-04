e56d4e017a28ce8aadcf98640a6c448ac9c59d7c
vienyevelyn <88517941+vienyevelyn@users.noreply.github.com>
2026-08-04 10:35:58 +0700

    Fixing function withdraw, deposit and creating transfer to validate

--- Account.kt
+++ Account.kt
@@ -10,16 +10,50 @@ class Account(
         }
         return false
     }
-    fun deposit(inputAmount : Int){
+    
+    fun deposit(inputAmount : Int) : Boolean{
         
         if (inputAmount > 0){
             balance += inputAmount
             println("Desposit Success")
+            println("Your Balance ${balance}")
+            return true
         }
         else{
             println("Deposit Failed")
+            println("Your Balance ${balance}")
+            return false
         }
-        println("Your Balance ${balance}")
+        
         
     }
+    
+    fun withdraw(inputAmount : Int) : Boolean{
+        if (inputAmount < balance){
+            balance -= inputAmount
+            println("Withdraw Success")
+            println("Your Balance ${balance}")
+            return true
+        }
+        else{
+            println("You can't withdraw all your money or more")
+            println("Your Balance ${balance}")
+            return false
+        }
+        
+    }
+    
+    fun transfer(receiver: Account, inputAmount: Int) : Boolean{
+        val withdrawMoney = withdraw(inputAmount)
+        if (withdrawMoney){
+            receiver.deposit(inputAmount)
+            println("Transfer success")
+            return true
+        }
+        else{
+            println("Transfer Failed")
+            return false
+        }
+    }
+    
 }
