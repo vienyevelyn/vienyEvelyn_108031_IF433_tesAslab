5785e0dec97c318c7a5ae41865a00b6a2075e360
vienyevelyn <88517941+vienyevelyn@users.noreply.github.com>
2026-08-04 10:40:48 +0700

    create class Bank and initialize

---
+++ Bank.kt
@@ -0,0 +1,11 @@
+class Bank{
+    private val accounts = mutableListOf<Account>()
+    
+    init {
+        accounts.add(Account("Angy", "0001", "01012006"))
+        accounts.add(Account("Bandy", "0002", "02022006"))
+        accounts.add(Account("Chonky", "0003", "03032006"))
+    }
+    
+
+}
